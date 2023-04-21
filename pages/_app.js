de914e229cfdd828fba50abad0ca026ab92e99d1
finnformica <finnformica@gmail.com>
2023-04-21 17:09:04 +0100

    add next link behaviour to menu items

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,28 @@ import { CssBaseline, ThemeProvider, createTheme } from "@mui/material";
 import Layout from "../layout/layout";
 import MetaTags from "../components/MetaTags/MetaTags";
 
+import NextLink from "next/link";
+import { forwardRef } from "react";
+
+const LinkBehaviour = forwardRef(function LinkBehaviour(props, ref) {
+  return <NextLink ref={ref} {...props} />;
+});
+
 function MyApp({ Component, pageProps }) {
-  const theme = createTheme();
+  const theme = createTheme({
+    components: {
+      MuiLink: {
+        defaultProps: {
+          component: LinkBehaviour,
+        },
+      },
+      MuiButtonBase: {
+        defaultProps: {
+          LinkComponent: LinkBehaviour,
+        },
+      },
+    },
+  });
 
   return (
     <>
