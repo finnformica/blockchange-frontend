7b5929e410176a65ec406ea5c8269ad2f8f0a5fa
finnformica <finnformica@gmail.com>
2023-04-21 18:51:51 +0100

    add page for causes

---
+++ pages/view/[slug]/index.js
@@ -0,0 +1,11 @@
+import { Typography, Container } from "@mui/material";
+
+const CausePage = () => {
+  return (
+    <Container maxWidth="lg">
+      <Typography variant="h1">My Cause</Typography>
+    </Container>
+  );
+};
+
+export default CausePage;
