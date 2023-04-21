95f601c8d2a7dd253a06385ef86e7bc3a685c542
finnformica <finnformica@gmail.com>
2023-04-21 16:36:59 +0100

    make header responsive

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import {
   AppBar,
   Toolbar,
@@ -5,38 +7,157 @@ import {
   Box,
   IconButton,
   Button,
+  Container,
+  Menu,
+  MenuItem,
 } from "@mui/material";
 import { FaHands } from "react-icons/fa";
-
+import { HiOutlineMenuAlt1 } from "react-icons/hi";
 import Image from "next/image";
 
 const Header = () => {
+  const pages = ["View", "Create", "FAQs", "About"];
+
+  const [anchorElNav, setAnchorElNav] = useState(null);
+  const [anchorElUser, setAnchorElUser] = useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
   return (
     <Box>
       <AppBar position="static" color="secondary">
-        <Toolbar>
-          <IconButton aria-label="blockchange logo" sx={{ mr: 0.5 }}>
-            <FaHands size={"1.8rem"} />
-          </IconButton>
-          <Typography variant="h5" sx={{ flexGrow: 1 }}>
-            BlockChange
-          </Typography>
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Typography
+              variant="h5"
+              noWrap
+              component="a"
+              href="/"
+              sx={{
+                mr: 2,
+                display: { xs: "none", md: "flex" },
+                fontWeight: 700,
+                letterSpacing: ".1rem",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              <FaHands size="1.8rem" style={{ marginRight: 10 }} />
+              BlockChange
+            </Typography>
 
-          <Button
-            variant="contained"
-            color={"error"}
-            startIcon={
-              <Image
-                src="/metamask.svg"
-                alt="Metamask Logo"
-                width={20}
-                height={20}
-              />
-            }
-          >
-            Connect
-          </Button>
-        </Toolbar>
+            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleOpenNavMenu}
+                color="inherit"
+              >
+                <HiOutlineMenuAlt1 />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorElNav}
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                open={Boolean(anchorElNav)}
+                onClose={handleCloseNavMenu}
+                sx={{
+                  display: { xs: "block", md: "none" },
+                }}
+              >
+                {pages.map((page) => (
+                  <MenuItem key={page} onClick={handleCloseNavMenu}>
+                    <Typography textAlign="center">{page}</Typography>
+                  </MenuItem>
+                ))}
+              </Menu>
+            </Box>
+
+            <Typography
+              variant="h5"
+              noWrap
+              component="a"
+              href=""
+              sx={{
+                mr: 2,
+                display: { xs: "flex", md: "none" },
+                flexGrow: 1,
+                fontWeight: 700,
+                letterSpacing: ".1rem",
+                color: "inherit",
+                textDecoration: "none",
+              }}
+            >
+              <FaHands size="1.8rem" style={{ marginRight: 10 }} />
+              BlockChange
+            </Typography>
+
+            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+              {pages.map((page) => (
+                <Button
+                  key={page}
+                  onClick={handleCloseNavMenu}
+                  sx={{ my: 2, color: "white", display: "block" }}
+                >
+                  {page}
+                </Button>
+              ))}
+            </Box>
+
+            <Box sx={{ flexGrow: 0 }}>
+              <Button
+                variant="contained"
+                color={"error"}
+                sx={{ display: { xs: "none", md: "flex" }, flexGrow: 1 }}
+                startIcon={
+                  <Image
+                    src="/metamask.svg"
+                    alt="Metamask Logo"
+                    width={20}
+                    height={20}
+                  />
+                }
+              >
+                Connect
+              </Button>
+              <Button
+                variant="contained"
+                color={"error"}
+                sx={{ display: { xs: "flex", md: "none" }, flexGrow: 1 }}
+              >
+                <Image
+                  src="/metamask.svg"
+                  alt="Metamask Logo"
+                  width={20}
+                  height={20}
+                />
+              </Button>
+            </Box>
+          </Toolbar>
+        </Container>
       </AppBar>
     </Box>
   );
