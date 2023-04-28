59b506c29aabc56cf3401fb16272151e3d76d24e
finnformica <finnformica@gmail.com>
2023-04-28 18:23:11 +0100

    separate content from code

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -21,7 +21,7 @@ import Image from "next/image";
 import { useWeb3React } from "@web3-react/core";
 import { InjectedConnector } from "@web3-react/injected-connector";
 
-const pages = ["View", "Create", "FAQs", "About"];
+import { pages } from "../../constants/pages";
 
 const Header = () => {
   const [hasMetamask, setHasMetamask] = useState(false);
