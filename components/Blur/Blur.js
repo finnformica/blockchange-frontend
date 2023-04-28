d8fcfae2afa6dba1412d0dec2ec8e28e5c1b273a
finnformica <finnformica@gmail.com>
2023-04-28 14:49:20 +0100

    adjust positions of blurs

--- components/Blur/Blur.js
+++ components/Blur/Blur.js
@@ -3,24 +3,22 @@ import Image from "next/image";
 
 const Blur = () => {
   return (
-    <>
-      <Box sx={{ position: "absolute", left: 0, top: -450, zIndex: -1 }}>
-        <Image
-          src="/imgs/blur.svg"
-          alt="background blur"
-          width={900}
-          height={900}
-        />
-      </Box>
-      <Box sx={{ position: "absolute", left: 700, top: 0, zIndex: -1 }}>
-        <Image
-          src="/imgs/blur.svg"
-          alt="background blur"
-          width={900}
-          height={900}
-        />
-      </Box>
-    </>
+    <Box>
+      <Image
+        src="/imgs/blur.svg"
+        alt="background blur"
+        width={800}
+        height={800}
+        style={{ position: "absolute", top: -400, left: -50, zIndex: -1 }}
+      />
+      <Image
+        src="/imgs/blur.svg"
+        alt="background blur"
+        width={800}
+        height={800}
+        style={{ position: "absolute", top: 150, left: -350, zIndex: -1 }}
+      />
+    </Box>
   );
 };
 
