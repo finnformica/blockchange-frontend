89d6d2c5d148b60ed3860d496f2e91c1cedab52c
finnformica <finnformica@gmail.com>
2023-04-28 14:01:47 +0100

    update crossOrigin to jsx syntax

--- components/MetaTags/MetaTags.js
+++ components/MetaTags/MetaTags.js
@@ -13,7 +13,11 @@ const MetaTags = () => {
       <meta charSet="utf-8" />
 
       <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossOrigin="true"
+      />
       <link
         href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;500;600;700&display=swap"
         rel="stylesheet"
