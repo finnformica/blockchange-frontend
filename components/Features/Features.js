a0932d476409ecfab3b3e7516f4e00c24687d6be
finnformica <finnformica@gmail.com>
2023-04-28 15:12:25 +0100

    centre items and refactor code

--- components/Features/Features.js
+++ components/Features/Features.js
@@ -1,85 +1,94 @@
-import { Container, Box, Typography } from "@mui/material";
+import { Container, Box, Typography, useTheme } from "@mui/material";
 import Image from "next/image";
 import PillButton from "../PillButton/PillButton";
 
+const AnalyticsHeader = () => (
+  <Typography
+    variant="p"
+    style={{ color: "#98D5D5" }}
+    fontWeight={600}
+    fontSize={11}
+    letterSpacing=".1rem"
+  >
+    ANALYTICS
+  </Typography>
+);
+
+const FeatureImage = ({ src }) => (
+  <Image src={src} alt="Image of feature" width={250} height={250} />
+);
+
+const FeatureContent = ({ title, description, button }) => (
+  <Box>
+    <AnalyticsHeader />
+    <Typography
+      variant="h2"
+      fontFamily="Outfit"
+      fontWeight={600}
+      fontSize={36}
+      maxWidth={300}
+    >
+      {title}
+    </Typography>
+    <Typography fontSize={10} maxWidth={220} sx={{ pt: 1, pl: 0.5, mb: 2 }}>
+      {description}
+    </Typography>
+    {button ? (
+      <PillButton variant={button.variant}>{button.content}</PillButton>
+    ) : (
+      <></>
+    )}
+  </Box>
+);
+
+const FeatureContainer = ({ children }) => (
+  <Box
+    sx={{
+      display: "flex",
+      flexDirection: "row",
+      gap: { xs: 4, sm: 8, md: 16, lg: 24 },
+      margin: "auto",
+    }}
+  >
+    {children}
+  </Box>
+);
+
 const Features = () => {
   return (
-    <Container maxWidth={false}>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          gap: 6,
-        }}
-      >
-        <Image
-          src="/imgs/feature1.svg"
-          alt="Image of feature"
-          width={250}
-          height={250}
+    <Container
+      maxWidth={false}
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+      }}
+    >
+      <FeatureContainer>
+        <FeatureImage src="/imgs/feature1.svg" />
+        <FeatureContent
+          title="Built-In Analytics To Track Your NFTs"
+          description="Use our built-in analytics dashboard to pull valuable insights and monitor the value of your BlockChange portfolio over time."
+          button={{ variant: "outlined", content: "View" }}
         />
-        <Box>
-          <Typography variant="p" style={{ color: "#98D5D5" }} fontWeight={500}>
-            ANALYTICS
-          </Typography>
-          <Typography
-            variant="h2"
-            fontFamily="Outfit"
-            fontWeight={600}
-            fontSize={36}
-            maxWidth={300}
-          >
-            Built-In Analytics To Track Your NFTs
-          </Typography>
-          <Typography
-            fontSize={10}
-            maxWidth={220}
-            sx={{ pt: 1, pl: 0.5, mb: 2 }}
-          >
-            Use our built-in analytics dashboard to pull valuable insights and
-            monitor the value of your BlockChange portfolio over time.
-          </Typography>
-          <PillButton variant="outlined">View</PillButton>
-        </Box>
-      </Box>
+      </FeatureContainer>
 
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          gap: 6,
-        }}
-      >
-        <Box>
-          <Typography variant="p" style={{ color: "#98D5D5" }} fontWeight={500}>
-            ANALYTICS
-          </Typography>
-          <Typography
-            variant="h2"
-            fontFamily="Outfit"
-            fontWeight={600}
-            fontSize={36}
-            maxWidth={300}
-          >
-            Built-In Analytics To Track Your NFTs
-          </Typography>
-          <Typography
-            fontSize={10}
-            maxWidth={220}
-            sx={{ pt: 1, pl: 0.5, mb: 2 }}
-          >
-            Use our built-in analytics dashboard to pull valuable insights and
-            monitor the value of your BlockChange portfolio over time.
-          </Typography>
-          <PillButton variant="contained">Create</PillButton>
-        </Box>
-        <Image
-          src="/imgs/feature2.svg"
-          alt="Image of feature"
-          width={250}
-          height={250}
+      <FeatureContainer>
+        <FeatureContent
+          title="Built-In Analytics To Track Your NFTs"
+          description="Use our built-in analytics dashboard to pull valuable insights and monitor the value of your BlockChange portfolio over time."
+          button={{ variant: "contained", content: "Create" }}
         />
-      </Box>
+        <FeatureImage src="/imgs/feature2.svg" />
+      </FeatureContainer>
+
+      <FeatureContainer>
+        <FeatureImage src="/imgs/feature3.svg" />
+        <FeatureContent
+          title="Built-In Analytics To Track Your NFTs"
+          description="Use our built-in analytics dashboard to pull valuable insights and monitor the value of your BlockChange portfolio over time."
+        />
+      </FeatureContainer>
     </Container>
   );
 };
