5503c10aa46a2368660ef658304d59d028387663
finnformica <finnformica@gmail.com>
2023-04-29 17:29:05 +0100

    stick footer to bottom of page

--- layout/Layout.js
+++ layout/Layout.js
@@ -1,15 +1,17 @@
+import { Container } from "@mui/material";
+
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 import Blur from "../components/Blur/Blur";
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <Container disableGutters maxWidth={false} style={{ minHeight: "100vh" }}>
       <Header />
       <Blur />
       <main>{children}</main>
       <Footer />
-    </>
+    </Container>
   );
 };
 
