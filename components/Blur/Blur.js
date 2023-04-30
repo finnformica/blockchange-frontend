c15a1854c2d17abe6a6bb937ed17c2266c04af02
finnformica <finnformica@gmail.com>
2023-04-30 16:59:27 +0100

    add blur using css styles instead of svg

--- components/Blur/Blur.js
+++ components/Blur/Blur.js
@@ -1,23 +1,47 @@
 import { Box } from "@mui/material";
-import Image from "next/image";
 
 const Blur = () => {
   return (
     <Box>
-      <Image
-        src="/imgs/blur.svg"
-        alt="background blur"
-        width={800}
-        height={800}
-        style={{ position: "absolute", top: -400, left: -50, zIndex: -1 }}
-      />
-      <Image
-        src="/imgs/blur.svg"
-        alt="background blur"
-        width={800}
-        height={800}
-        style={{ position: "absolute", top: 200, left: -400, zIndex: -1 }}
-      />
+      <Box
+        sx={{
+          "&::before": {
+            background:
+              "linear-gradient(to bottom right, rgba(152, 213, 213, 0), rgba(152, 213, 213, 0.2), rgba(152, 213, 213, 0.8))",
+            content: '""',
+            borderRadius: "50%",
+            width: "700px",
+            height: "700px",
+            marginLeft: "-700px",
+            marginTop: "-350px",
+            left: "75%",
+            position: "absolute",
+            filter: "blur(100px)",
+            transform: "translateZ(0)",
+            zIndex: -1,
+          },
+        }}
+      ></Box>
+
+      <Box
+        sx={{
+          "&::before": {
+            background:
+              "linear-gradient(rgba(152, 213, 213, 0), rgba(152, 213, 213, 1))",
+            content: '""',
+            borderRadius: "50%",
+            width: "800px",
+            height: "800px",
+            marginLeft: "-900px",
+            marginTop: "200px",
+            left: "25%",
+            position: "absolute",
+            filter: "blur(150px)",
+            transform: "translateZ(0)",
+            zIndex: -1,
+          },
+        }}
+      ></Box>
     </Box>
   );
 };
