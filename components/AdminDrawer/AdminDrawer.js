75a452992cd3746363760e9c03db4faf392528b9
finnformica <finnformica@gmail.com>
2023-05-05 15:21:50 +0100

    add admin panel drawer

---
+++ components/AdminDrawer/AdminDrawer.js
@@ -0,0 +1,81 @@
+import { useState } from "react";
+import {
+  Button,
+  Drawer,
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Divider,
+} from "@mui/material";
+
+const AdminDrawer = () => {
+  const [state, setState] = useState(false);
+
+  const toggleDrawer = (open) => (event) => {
+    if (
+      event.type === "keydown" &&
+      (event.key === "Tab" || event.key === "Shift")
+    ) {
+      return;
+    }
+
+    setState(open);
+  };
+
+  const content = () => (
+    <Box
+      sx={{ width: 250 }}
+      role="presentation"
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <List>
+        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+      <List>
+        {["All mail", "Trash", "Spam"].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton>
+              <ListItemText primary={text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <>
+      <Button sx={{ pl: 2 }} onClick={toggleDrawer(true)}>
+        Admin panel
+      </Button>
+      <Drawer
+        anchor="right"
+        open={state}
+        onClose={toggleDrawer(false)}
+        PaperProps={{
+          sx: {
+            backgroundColor: "#0D111C",
+            border: "1px solid #777",
+            borderRadius: "10px",
+            height: "375px",
+            m: 1,
+          },
+        }}
+      >
+        {content()}
+      </Drawer>
+    </>
+  );
+};
+
+export default AdminDrawer;
