c31f8a339684fab7646165be06ab49a1353976e1
finnformica <finnformica@gmail.com>
2023-05-06 19:16:46 +0100

    add button for admin panel and allow donations

--- pages/view/[slug].js
+++ pages/view/[slug].js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Box, Container, Typography } from "@mui/material";
 import Image from "next/image";
 import { useRouter } from "next/router";
@@ -11,28 +12,77 @@ import CauseTrust from "../../components/CauseTrust/CauseTrust";
 
 import contractInfo from "../../constants/contractInfo";
 import { ethers } from "ethers";
+import AdminDrawer from "../../components/AdminDrawer/AdminDrawer";
 
-const instantiateContractFactory = () => {
+const instantiateContract = (address, abi) => {
   const url = "http://localhost:8545";
   const provider = new ethers.providers.JsonRpcProvider(url);
   const signer = provider.getSigner();
-  const contract = new ethers.Contract(
-    contractInfo.address,
-    contractInfo.abi,
-    signer
-  );
+  const contract = new ethers.Contract(address, abi, signer);
 
   return contract;
 };
 
-const donate = async (causeId, amount) => {};
+const donate = async (address, amount) => {
+  try {
+    const { ethereum } = window;
+
+    if (ethereum !== undefined) {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+
+      const contract = new ethers.Contract(
+        address,
+        contractInfo.contract_abi,
+        signer
+      );
+
+      const tx = await contract.donate({
+        value: ethers.utils.parseEther("1"),
+        gasLimit: 3000000,
+      });
+
+      console.log(tx);
+    } else {
+      console.log("Ethereum object not found");
+    }
+  } catch (e) {
+    console.log(e);
+  }
+};
 
 const CausePage = ({ cause }) => {
   const router = useRouter();
+  const [admin, setAdmin] = useState(false);
 
+  // display loading if page is not generated yet
   if (router.isFallback) {
     return <h1>Loading...</h1>;
   }
+
+  // check if user is admin of cause
+  useEffect(() => {
+    if (typeof window.ethereum !== "undefined") {
+      const fetchAccounts = async () => {
+        const accounts = await window.ethereum.request({
+          method: "eth_accounts",
+        });
+
+        if (accounts.length == 0) {
+          setAdmin(false);
+          return;
+        }
+
+        setAdmin(accounts[0].toLowerCase() == cause.admin.toLowerCase());
+      };
+
+      fetchAccounts().catch((e) => console.log(e));
+
+      window.ethereum.on("accountsChanged", function (accounts) {
+        fetchAccounts().catch((e) => console.log(e));
+      });
+    }
+  }, []);
 
   return (
     <Container maxWidth="lg">
@@ -53,8 +103,9 @@ const CausePage = ({ cause }) => {
               <SmallTitle>Cause</SmallTitle>
               <StateChip
                 cause={cause}
-                color={cause.causeState == "2" ? "#61EF61" : "#E10600"}
+                color={cause.causeState == 1 ? "#61EF61" : "#E10600"}
               />
+              {admin ? <AdminDrawer /> : <></>}
             </Box>
             <BigTitle fontSize={48}>{cause.title}</BigTitle>
             <Typography fontSize={14} sx={{ pt: 1 }}>
@@ -79,7 +130,8 @@ const CausePage = ({ cause }) => {
         <Box sx={{ display: "flex", gap: 2, mt: 4 }}>
           <PillButton
             variant="contained"
-            {...(cause.causeState == "2" ? {} : { disabled: true })}
+            {...(cause.causeState == 1 ? {} : { disabled: true })}
+            onClick={() => donate(cause.address, 2)}
           >
             Donate
           </PillButton>
@@ -98,7 +150,10 @@ const CausePage = ({ cause }) => {
 };
 
 export const getStaticPaths = async () => {
-  const contract = instantiateContractFactory();
+  const contract = instantiateContract(
+    contractInfo.factory_address,
+    contractInfo.factory_abi
+  );
 
   const res = await contract.functions.cfRetrieveIds();
 
@@ -113,7 +168,10 @@ export const getStaticPaths = async () => {
 };
 
 export const getStaticProps = async ({ params }) => {
-  const contract = instantiateContractFactory();
+  const contract = instantiateContract(
+    contractInfo.factory_address,
+    contractInfo.factory_abi
+  );
   try {
     const res = await contract.functions.cfRetrieveInfo(params.slug);
     const causeInfo = res[0];
@@ -122,6 +180,7 @@ export const getStaticProps = async ({ params }) => {
       id: causeInfo["id"],
       title: causeInfo["name"],
       admin: causeInfo["admin"],
+      address: causeInfo["contractAddress"],
       incoming: causeInfo["incoming"],
       outgoing: causeInfo["outgoing"],
       causeTotal: ethers.utils.formatEther(
