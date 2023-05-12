249f21bcbfa17a6a6635dfa27d331cc1d0a39e9a
finnformica <finnformica@gmail.com>
2023-05-12 11:29:55 +0100

    add styling and input field to modal

--- components/ViewModal/ViewModal.js
+++ components/ViewModal/ViewModal.js
@@ -1,28 +1,46 @@
-import React from 'react'
-import {Box, Modal, Container, Typography} from "@mui/material";
+import { useState } from "react";
+import { Box, Modal, TextField } from "@mui/material";
+import PillButton from "../PillButton/PillButton";
 
-const ViewModal = ({open, handleClose}) => {
+const ViewModal = ({ open, handleClose }) => {
+  const [id, setId] = useState("");
+
   return (
     <Modal open={open} onClose={handleClose}>
-        <Box
-          sx={{
-            position: "absolute",
-            top: "50%",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            p: 2,
-            bgcolor: "background.paper"
+      <Box
+        sx={{
+          position: "absolute",
+          top: "50%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          p: 2,
+          width: 300,
+          height: 200,
+          backgroundColor: "#010135",
+          border: "0.5px solid #909bbc",
+          borderRadius: "10px",
+          textAlign: "center",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-evenly",
+          alignItems: "center",
+        }}
+      >
+        <TextField
+          label="Cause ID"
+          variant="outlined"
+          type="text"
+          value={id}
+          onChange={(e) => {
+            setId(e.target.value);
           }}
-        >
-          <Typography variant="h6" component="h2" gutterBottom>
-            Text in a modal
-          </Typography>
-          <Typography variant="body1" gutterBottom>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
-        </Box>
-      </Modal>
-  )
-}
+        />
+        <PillButton variant="outlined" sx={{ mt: 2 }} href={`/view/${id}`}>
+          View
+        </PillButton>
+      </Box>
+    </Modal>
+  );
+};
 
-export default ViewModal
+export default ViewModal;
